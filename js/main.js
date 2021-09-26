5efad62ec72e24e65bd810f3c3b8d32f9b4118bd
mb1371 <muuhamed.mb@gmail.com>
2021-09-26 10:55:30 +0100

    modified files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("nav").style.display = "block";
+}
+  
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("nav").style.display = "none";
+}
